c66b07b660479d3f7fe990b937cac8c688578b7c
Benjamin Spain <bspain@trackhs.com>
2018-05-31 17:42:42 -0500

    Calc

---
+++ src/Calc.ts
@@ -0,0 +1,8 @@
+export default class Calc {
+    public add(num1: number, num2: number) : number {
+      return num1 + num2;
+    }
+    public subtract(num1: number, num2: number) : number {
+      return num1 - num2;
+    }
+  }
